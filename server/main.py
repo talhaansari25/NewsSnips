05905b4f8fc63d8c63b226063ccba270f9baa3b5
Talha <talhaansari2026@gmail.com>
2025-04-28 16:05:05 +0530

    .

--- server/main.py
+++ server/main.py
@@ -3,3 +3,7 @@ from app import app
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
+
+# For Vercel to recognize the app
+def handler(environ, start_response):
+    return app(environ, start_response)
