42c7436680de6ced089d3c51a242e81f718f4d0f
Talha <talhaansari2026@gmail.com>
2025-04-30 23:43:04 +0530

    .

--- server/app/category.py
+++ server/app/category.py
@@ -24,30 +24,4 @@ def myCategory(query):
     return my_cat_art
 
 
-# import os
-# import pandas as pd
-# import spacy
-
-# nlp = spacy.load("en_core_web_md")
-
-# current_directory = os.path.dirname(os.path.abspath(__file__))
-# file_path = os.path.join(current_directory, "FinalData.csv")
-# file_path = os.path.normpath(file_path)
-# data = pd.read_csv(file_path)
-
-# def get_cat_nlp(keywords):
-#     keywords_doc = [nlp(kw.lower()) for kw in keywords]
-#     relevant_articles = []
-
-#     for _, row in data.iterrows():
-#         category_doc = nlp(row["category"].lower())
-#         similarity_scores = [category_doc.similarity(kw_doc) for kw_doc in keywords_doc]
-
-#         if max(similarity_scores) > 0.7:
-#             relevant_articles.append(row.to_dict())
-
-#     return relevant_articles
-
-# def myCategory(query):
-#     return get_cat_nlp(query)
 
